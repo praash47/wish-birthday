f459a10ad5f1d6213b7266e50fd6878c03839506
aasis <45727645+praash47@users.noreply.github.com>
2023-08-26 23:57:54 +0545

    .
    .

---
+++ script.js
@@ -0,0 +1,264 @@
+let questionsAnswers = [
+    {
+        "Question": "What are you thinking right now?",
+        "Answers": [
+            "I am thinking about myself in a year",
+            "I am thinking about my today's plans",
+            "I am thinking about my family",
+            "I am thinking about him"
+        ],
+        "Correct Answer": "",
+        "Secret Message": "Whatever you are thinking; may this auspicious day bring your wishes to fulfilled state in life!",
+        "Read Interval": 10
+    },
+    {
+        "Question": "The melody of the world-famous song “Happy Birthday to Your” actually comes from which song?",
+        "Answers": [
+            "“Happy New Year”",
+            "“Good Morning to All”",
+            "“Merry Christmas”"
+        ],
+        "Correct Answer": 1,
+        "Secret Message": "First of all, for easing your patience, there are all together of 16 questions in this quiz. The response ain't going anywhere. Why this? A small burst of knowledge for you; through a random question set that I found on the internet. I wanted to refresh my JS knowledge, I never made a full fleged application with pure vanilla JS. I also wanted to share some new perspectives on Birthday as well to you. A sort of intellectual gift.",
+        "Read Interval": 25
+    },
+    {
+        "Question": "Mainly celebrated in Canada and the United States, a sweet sixteen is a party to celebrate a teenager’s birthday when they turn how many years old?",
+        "Answers": [
+            "16",
+            "18",
+            "21"
+        ],
+        "Correct Answer": 0,
+        "Secret Message": "This has almost no any copied code except syntaxes. Chat GPT is just used for just formatting the data into array of objects. First of all, before going to the perspectives, the problem that you are facing; anger. A small piece of advice/knowledge I sourced from somewhere regarding anger; which is also my problem, I have noted in my diay: Here it goes: Anger poisons the mind, turning you into a venomous snake. You start biting those around you, hurting them & causing them to avoid you. Anger clouds our judgement, wants to do nothing but recriprocate pain.",
+        "Read Interval": 40
+    },
+    {
+        "Question": "Which of the following ingredients is not normally used in a birthday cake?",
+        "Answers": [
+            "Sugar",
+            "Eggs",
+            "Raisins"
+        ],
+        "Correct Answer": 2,
+        "Secret Message": "Really sorry for making this long actually, I started it at 9 PM, took 2 hours to build the backbone. It's 11 PM now. Thanks, a small secret: you have also became my motivation to work since here at LIS. Hahah, leave that; I know I am boring. I actually planned to spend hours finding you the best and most unique perspectives to see a birthday in life. The regular perspective regarding the birthday is that I will do this or that from the birthday. But, let us just try to think differently on this. It's 11.15 PM, I am borrowing from ChatGPT. It has returned a lots of perspectives. I will give you only the ones that I loved.",
+        "Read Interval": 30
+    },
+    {
+        "Question": "What happened to William Shakespeare on his 52nd birthday?",
+        "Answers": [
+            "He completed a new play",
+            "He made an excursion around England",
+            "He died"
+        ],
+        "Correct Answer": 2,
+        "Secret Message": "I know this may be weird for you. I planned to think for 3-4 hours to provide you with best ones, but will be unable. An alternative perspective to see it would be a reminder of life's fragility: I remember a quote from Yudisthir, we see death everywhere around us; but yet we live as immortals; According to my mom, birthday is a sensitive day; where some bad things may happen. Actually, this holds true only for certain ages which I don't think you lie in right now. But, yet this reminds us that life is very fragile and anything may happen at any moment. So, let's be grateful to our loved ones, and be grateful to god for whatever we have!?",
+        "Read Interval": 45
+    },
+    {
+        "Question": "It is a tradition in Canada that friends can rub butter on which part of your body on your birthday?",
+        "Answers": [
+            "Nose",
+            "Cheek",
+            "Head"
+        ],
+        "Correct Answer": 0,
+        "Secret Message": "Uff, this is tough, right? A New beginning: This is but a similar perspective to the most general one: but, new beginning can be to reset our existence, a new beginning for our relationships? To try to see even deeper inner beauty of loved ones around us. A time to leave the past; if it is dreaded? A new beginning but should have the with the mistakes and lessons learnt in the past. I really hope to you to be posting story of NYC next birthday and pray for the same to god on this moment.",
+        "Read Interval": 40
+    },
+    {
+        "Question": "In South Korea, which of the following is a traditional birthday dish at breakfast?",
+        "Answers": [
+            "Seaweed soup",
+            "Pickled eggs",
+            "Roast turnip cake"
+        ],
+        "Correct Answer": 0,
+        "Secret Message": "I just don't like easy gifts or non-authentic ones that is just buy and give. Instead of laying down the new year or birthday resolutions, how about laying down the problems you have within yourself and creating a plan in order to solve those problems? I think this would give inner peace and also satisfaction and not just satisfaction like the resolutions give you.",
+        "Read Interval": 25
+    },
+    {
+        "Question": "When is Martin Luther King Jr. Day, which marks the birthday of the American activist?",
+        "Answers": [
+            "January 15",
+            "February 20",
+            "March 10"
+        ],
+        "Correct Answer": 0,
+        "Secret Message": "I just ran out of ChatGPT Ideas and I have compiled many chatGPT ideas into one perspective that I felt nice. In the internet among the boring stuff, I just stumbled upon an article which is giving a Vedic perspective on birthday. From the Vedic perspective, One’s birthday is considered to be a very important day in connection with one’s longevity; not entertainment or enjoyment. There are three suggestions given by the writer: Seek blessings from elders and well-wishers, Visit a temple nearby and Give something in charity",
+        "Read Interval": 35
+    },
+    {
+        "Question": "To mark the birthday of Mahatma Gandhi, which shops are closed in India on October 2?",
+        "Answers": [
+            "Tobacco shops",
+            "Liquor shops",
+            "Lotto shops"
+        ],
+        "Correct Answer": 1,
+        "Secret Message": "Seek blessings from elders and well-wishers: It's quite of a coincidence that I did this every birthday and at least once every 3 months of life. This is one of the best things that I like about the Eastern cultural society. Gaining blessings from the elders and well wishers in my perspective induces spiritual well being and a sense of securedness. Our religion attributes a lot of power to it. Visiting a Temple nearby: Mom sends me to visit so many temples for me: but, visiting the temple could be for: thanking god for life, wishing for an amazing, easy and fruitful life till next birthday, ahahah.",
+        "Read Interval": 40
+    },
+    {
+        "Question": "What is the age of majority in the UK when you officially become an adult?",
+        "Answers": [
+            "16",
+            "18",
+            "20"
+        ],
+        "Correct Answer": 1,
+        "Secret Message": "Give something in charity: I didn't give it this birthday though, but, last birthday, I gave away a sack of rice to people who have a hard time; you can also do some charity during the birthday. It would be an act of selfless kindness but will make the world a better place to live in for someone else. Wow, how beautiful would it be that your existence caused some smile and some added push to their life. Charity passes on really well and also passes on like a chain effect of Nuclear Fission if went onto the hands of right person.",
+        "Read Interval": 30
+    },
+    {
+        "Question": "In India and Nepal, why is the head of a child shaved on their first birthday?",
+        "Answers": [
+            "To prevent diseases",
+            "To cleanse evils in their past lives",
+            "To free from the materialistic world"
+        ],
+        "Correct Answer": 1,
+        "Secret Message": "Wow, this question itself is amazing. I didn't know about that. But, cleansing evil is really interesting. I recently realized that each of us are born with some innate evilness; which is something intrapersonal rather than outside ghosts or evil. A birthday should be a drive to cleanse away at least a few percent of our evil and dark side. Diwash once told me that 'Life gives chain of reactions upon changing even one aspect of it' i.e. a lots of aspects of life are so interconnected and twined with each other. Cleansing evil in oneself may also give some positive ripple effects on other parts of life. ",
+        "Read Interval": 40
+    },
+    {
+        "Question": "Marilyn Monroe sang her own version of “Happy Birthday” to which U.S President?",
+        "Answers": [
+            "Richard Nixon",
+            "Lyndon Johnson",
+            "John F. Kennedy"
+        ],
+        "Correct Answer": 2,
+        "Secret Message": "I know it may be going boring, but bare with me for a bit now. There are 5 questions, I really wanted to write everything and didn't want to copy paste anything, but I don't want to miss deadline of wishing to you. It won't be fun and the whole point of this won't be just fulfilled. I will just now paste from 2 articles that I found. I will only paste the things that I have found that made me think. the poor and uneducated people neither remember their birthdays nor have they money to celebrate it. Rather, they are dying every year without a tablet of Paracetamol.And it is very hard for us to get any information regarding this issue. It means that celebrating birthday is not a compulsion but an option.",
+        "Read Interval": 45
+    },
+    {
+        "Question": "What do you wear when you wear a “birthday suit”?",
+        "Answers": [
+            "Bikini",
+            "Vest",
+            "None of the above"
+        ],
+        "Correct Answer": 2,
+        "Secret Message": "It is said that every person in this world is supposed to have come by taking certain kinds of traits to fulfill his or her mission. So, we humans should be committed as well asdedicated to accomplish our own missions. Therefore, while celebrating birthday, we should not forget our mission of life at any cost. Well, let’s think for a while, when a baby is born in a family, extra happiness is seen on the faces of all family members especially on the face of the mother. There is a saying in this regard that giving birth to baby is one of the biggest dream of every woman. So, every mother should be considered as the living god per our eastern culture. So, she should always be respected whether she is ugly, poor, uneducated and so on.",
+        "Read Interval": 40
+    },
+    {
+        "Question": "From which country did the tradition of sending cards on birthdays come from?",
+        "Answers": [
+            "England",
+            "Germany",
+            "France"
+        ],
+        "Correct Answer": 0,
+        "Secret Message": "Hello, it's me again back for a brief time; the previous sentence is sensitive to me; the capability of woman to give birth to a baby; what a wonder. I never could think that birthday could be thought of as this way as well. ",
+        "Read Interval": 40
+    },
+    {
+        "Question": "Which English poet published a collection of poetry called “Birthday Letters” only a few months before his death in 1998?",
+        "Answers": [
+            "Lord Byron",
+            "Ted Hughes",
+            "Thomas Gray"
+        ],
+        "Correct Answer": 1,
+        "Secret Message": "Higher and middle class people celebrate or may celebrate birthday grandly or moderately but lower class people may not celebrate it because of financial problem in their family. They must be satisfied with themselves or in other word they must do compromise. Not only on birthday, but they must do compromise in daily life as well. Certainly, a birthday is special day for everyone but all the people do not have a fate to enjoy it. Here, we shouldn’t forget that birthday celebration relates to mental thing rather than physical one i.e. medium of happiness. If we are happy from within, no need to celebrate the birthday because we at a time hear that some people first celebrate the birthday grandly per their financial capacity and commit a suicide later in the same day.",
+        "Read Interval": 50
+    },
+    {
+        "Question": "In the animated movie “Toy Story”, which present does Andy receive for his birthday?",
+        "Answers": [
+            "Woody",
+            "Mr. Potato Head",
+            "Buzz Lightyear"
+        ],
+        "Correct Answer": 2,
+        "Secret Message": "Hello, me back again. It's already 11.53 now. “No one is useless in this world who lightens the burden of another”- Charles Dickens. Okay, so now there was a research paper but we couldn't just discuss it over here. I seriously don't want to miss the deadline. I hope that you will excel so much in life. Hope that you get an amazing university soon and visa for the US or any other country soon. Rest, the life is yet to explore. Sorry if there were glitches in the application, didn't get the time to test. Hahah, smile!!! ",
+        "Read Interval": 35
+    }
+];
+
+
+
+let currentQuestionIndex = 0;
+let questionHTMLBox = document.querySelectorAll('.question')[0];
+let form = document.querySelector('form')
+
+let answers;
+
+let responses = []
+
+let renderQuestion = () => {
+    if (currentQuestionIndex === questionsAnswers.length) {
+        questionHTMLBox.innerHTML += "<h1>Hooray! The quiz is complete!<br><br></h1>"
+
+        questionHTMLBox.innerHTML += "Your answers: <br>"
+        responses.map((response, index) => {
+            questionHTMLBox.innerHTML += `${index+1}: ${response}<br>`
+        })
+
+        return
+    }
+    answers = questionsAnswers[currentQuestionIndex].Answers;
+
+    questionHTMLBox.innerHTML += `
+    <h2>
+    ${currentQuestionIndex + 1}. 
+    ${questionsAnswers[currentQuestionIndex].Question}
+    </h2>
+    <ul>
+    ${
+        answers.map((answer, index) => {
+            return `<li><input type="radio" name="selectedAnswer" value="${index}">${answer}</li>`
+        })
+    }
+    </ul>
+    `;
+}
+
+window.onload = () => {
+    renderQuestion();
+}
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+
+    var answerRadios = document.getElementsByName('selectedAnswer');
+
+    let answerSelected = false
+    for (let i = 0; i < answerRadios.length; i++) {
+        if(answerRadios[i].checked) {
+            if (questionsAnswers[currentQuestionIndex]["Correct Answer"] !== "") {
+                console.log(answerRadios[i].value)
+                if (answerRadios[i].value == questionsAnswers[currentQuestionIndex]["Correct Answer"]) {
+                    questionHTMLBox.innerHTML += "Yay, you're correct! Smart Girl!<br>"
+                } else {
+                    questionHTMLBox.innerHTML += `Nice try! The correct answer is ${questionsAnswers[currentQuestionIndex].Answers[questionsAnswers[currentQuestionIndex]["Correct Answer"]]}<br>`
+                }
+            } else {
+                questionHTMLBox.innerHTML += "Whatever you selected is correct! You selected it with your pure soul!<br>"
+            }           
+            responses.push(questionsAnswers[currentQuestionIndex].Answers[answerRadios[i].value])
+            questionHTMLBox.innerHTML += questionsAnswers[currentQuestionIndex]["Secret Message"] + '<br>'
+            answerSelected = true
+        }
+    }
+    if (!answerSelected) {
+        questionHTMLBox.innerHTML += "Please select an answer beautiful mind!<br>"
+        return
+    }
+    let timeleft = questionsAnswers[currentQuestionIndex]["Read Interval"]
+    questionHTMLBox.innerHTML += `<br>Heading to next question in <span id='timeLeft'>${timeleft}</span> seconds`
+    let timeleftHTML = document.querySelector('#timeLeft');
+    setInterval(() => {
+        timeleftHTML.innerHTML = timeleft;
+
+        if(timeleft === 0) {
+            currentQuestionIndex += 1;
+            questionHTMLBox.innerHTML = ""
+            renderQuestion();
+        }
+        timeleft -= 1;
+    }, 1000);
+    console.log(responses)
+}
